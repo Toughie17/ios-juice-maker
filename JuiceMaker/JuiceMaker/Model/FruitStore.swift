173827ad51b17d414caf96040f25f249b72796d3
Toughie <airj907@gmail.com>
2023-01-03 20:36:41 +0900

    Feat: STEP1 prototype

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -1,12 +1,26 @@
-//
-//  JuiceMaker - FruitStore.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-// 과일 저장소 타입
-class FruitStore {
+//과일 저장소 타입
+
+class FruitFeature {
+    private(set) var stock: Int = 10
+    
+    func addStock(_ num: Int) {
+        self.stock += num
+    }
+    
+    func subtractStock(_ num: Int) {
+        self.stock -= num
+    }
     
 }
+
+enum Fruits {
+    static let strawberry = FruitFeature()
+    static let banana = FruitFeature()
+    static let kiwi = FruitFeature()
+    static let pineapple = FruitFeature()
+    static let mango = FruitFeature()
+}
+
+//현재 각 과일객체에는 재고, 재고를 증가/감소 시키는 메소드가 있음.
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -1,12 +1,24 @@
-//
-//  JuiceMaker - JuiceMaker.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
+//딸기쥬스 딸기 16개
+//바나나쥬스 바나나 2개
+//키위쥬스 키위 3개
+//파인애플 쥬스 파인애플 2개
+//딸바쥬스 딸기10개 + 바나나 1개
+//망고 쥬스 망고 3개
+//망고키위쥬스 망고2개 키위 1개
 
-import Foundation
+enum juiceRecipe {
+    static let strawberryJuice = (Fruits.strawberry, 16)
+    static let bananaJuice = (Fruits.banana, 2)
+    static let kiwiJuice = (Fruits.kiwi, 3)
+    static let pineappleJuice = (Fruits.pineapple, 2)
+    static let strawberryBananaJuice = (Fruits.strawberry, 10, Fruits.banana, 1)
+    static let mangoJuice = (Fruits.mango, 3)
+    static let mangoKiwiJuice = (Fruits.mango, 2, Fruits.kiwi, 1)
+}
 
-// 쥬스 메이커 타입
+// 우선 위 조건대로 튜플에 담았는데 튜플에 담아야 하는 확실한 근거를 제시하기 어려운 상태
+//또한 2가지 이상의 과일이 섞였을 때 인덱싱 하기도 뭔가 불안함. 다른 컬렉션으로 담아야 할까?
+
 struct JuiceMaker {
     
 }
