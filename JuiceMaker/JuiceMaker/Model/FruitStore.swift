401df933e2f7cdcf249bf9f4e64a665b8670ea14
Toughie <airj907@gmail.com>
2023-01-04 13:45:04 +0900

    질문3

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -32,3 +32,27 @@ class FruitStore {
         fruitStock.updateValue(currentNumber - number, forKey: fruit)
     }
 }
+
+
+/*
+질문 3.
+- 과일의 재고를 관리하는 FruitStore를 class로 만든 이유는 무엇일까요?
+- 초기 재고 상수를 따로 만든 이유가 있을까요?
+- fruitStock 변수를 지연저장 프로퍼티(lazy)로 하신 이유가 있을까요?
+ 
+- lazy var fruitStock: [Fruit : Int] = {
+ var stock: [Fruit : Int] = [:]
+ Fruit.allCases.forEach { fruit in
+     stock[fruit] = defaultStock
+ }
+ return stock
+}()
+ 
+ 위 코드는 사실 이해가 잘 안되는데, 고차함수를 공부하고 다시 봐야겠죠..?
+ Fruit의 모든 케이스를 키값으로, 이 키들에 매칭되는 벨류에 10을 전부 넣어주는 코드인 것 같은데 정확히는 이해가 아직..ㅜㅜ
+ 
+ 
+- 과일수량 변경 함수에서 guard let 구문을 활용하신 이유가 있을까요? value가 nil일 가능성이 현재 있는지, 안전을 위해 작성하신 것인지 궁금합니다!
+- func use는 func add와 거의 유사한 함수네요. 우선 네이밍 같은 부분은 나중에 함께 고민해 보시죠 :)
+ 
+ */
